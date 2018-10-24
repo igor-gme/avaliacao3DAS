5167c272ede001214eaa94071c69eb23a1120e3d
Igor Gabriel <igorgabriel515@gmail.com>
2018-10-24 17:33:05 -0200

    Questao 2: Igor Gabriel - 150037074

--- Cadastros/CalcularDuracaoHoras.java
+++ Cadastros/CalcularDuracaoHoras.java
@@ -10,27 +10,47 @@ public class CalcularDuracaoHoras {
 	}
 	
 	public int ComputarHoras() {		
-		if (viagem.getHoraTermino() == viagem.getHoraInicio())
+		if (verificaHoraTerminoIgualHoraInicio())
 			duracaoHoras = 0;
-		if (viagem.getHoraTermino() > viagem.getHoraInicio()) //varias possibilidades... 
+		if (verificaHoraTerminoMaiorHoraInicio()) //varias possibilidades... 
 			if (viagem.getHoraTermino() == viagem.getHoraInicio() + 1) {  
-				if (viagem.getMinutosTermino() < viagem.getMinutoInicio())  //nao chegou a uma hora
+				if (verificaMinutoTerminoMenorMinutoInicio())  //nao chegou a uma hora
 					duracaoHoras = 0;
 				else //durou pelo menos uma hora
 					duracaoHoras = 1;
 			} else { //possivelmente ultrapassou duas horas
-				if (viagem.getHoraTermino() - viagem.getHoraInicio() > 2) //
-					duracaoHoras = viagem.getHoraTermino() - viagem.getHoraInicio();
-				else if (viagem.getHoraTermino() - viagem.getHoraInicio() == 2 &&   //certamente menos de duas horas  
-						 viagem.getMinutosTermino() < viagem.getMinutoInicio())    //e mais de uma hora
+				if (duracaoHorasPassadas() > 2) //
+					duracaoHoras = duracaoHorasPassadas();
+				else if (duracaoHorasPassadas() == 2 &&   //certamente menos de duas horas  
+						 verificaMinutoTerminoMenorMinutoInicio())    //e mais de uma hora
 					duracaoHoras = 1;
 				else //duracao de duas horas, certamente
 					duracaoHoras = 2;
 					
 			}
-		if (viagem.getHoraTermino() < viagem.getHoraInicio()) 
+		if (verificaHoraTerminoMenorHoraInicio()) 
 			duracaoHoras = -1; //para casos em que a hora de termino nao foi registrada
 		return duracaoHoras;
+	}
+
+	private boolean verificaHoraTerminoMenorHoraInicio() {
+		return viagem.getHoraTermino() < viagem.getHoraInicio();
+	}
+
+	private int duracaoHorasPassadas() {
+		return viagem.getHoraTermino() - viagem.getHoraInicio();
+	}
+
+	private boolean verificaMinutoTerminoMenorMinutoInicio() {
+		return viagem.getMinutosTermino() < viagem.getMinutoInicio();
+	}
+
+	private boolean verificaHoraTerminoMaiorHoraInicio() {
+		return viagem.getHoraTermino() > viagem.getHoraInicio();
+	}
+
+	private boolean verificaHoraTerminoIgualHoraInicio() {
+		return viagem.getHoraTermino() == viagem.getHoraInicio();
 	}
 
 }
--- Cadastros/CalcularDuracaoMinutos.java
+++ Cadastros/CalcularDuracaoMinutos.java
@@ -9,14 +9,22 @@ public class CalcularDuracaoMinutos {
 	}
 	
 	public int ComputarMinutos() {
-		if (viagem.getMinutosTermino() > viagem.getMinutoInicio()) 
-			duracaoMinutos = viagem.getMinutosTermino() - viagem.getMinutoInicio();
+		if (verificarMinutosTerminoEhMaiorMinutoInicio()) 
+			duracaoMinutos = calcularTempoDecorrido();
 		else {
 			duracaoMinutos = 60 - viagem.getMinutoInicio() + viagem.getMinutosTermino();
 			if (duracaoMinutos == 60) //caso especial
 				duracaoMinutos = 0;
 		}
 		return duracaoMinutos;
+	}
+
+	private int calcularTempoDecorrido() {
+		return viagem.getMinutosTermino() - viagem.getMinutoInicio();
+	}
+
+	private boolean verificarMinutosTerminoEhMaiorMinutoInicio() {
+		return viagem.getMinutosTermino() > viagem.getMinutoInicio();
 	}
 
 }
